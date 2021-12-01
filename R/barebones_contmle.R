176009834b8b5b44065095d4c189c070b977f26a
David Chen <david.chen49@berkeley.edu>
2021-11-30 22:33:48 -0800

    add barebones contmle outline

---
+++ R/barebones_contmle.R
@@ -0,0 +1,58 @@
+
+
+# data cleaning -------------------------------------------------------------------------------
+
+
+
+
+# parameter checking --------------------------------------------------------------------------
+
+# target time(s)
+# target event(s)
+# binary treatment
+# stopping criteria
+# ...
+
+
+# initial estimation / superlearning ----------------------------------------------------------
+
+## cross validation setup ----
+
+# loop
+{
+## train ----
+
+## predict ----
+}
+
+## metalearner ----
+
+## output initial estimates ----
+
+
+
+
+# Update step ---------------------------------------------------------------------------------
+
+# one-step tmle loop
+{
+## clever covariate (A and C) ----
+
+## clever covariate (Events) ----
+
+## fluctuation
+
+## update
+}
+
+
+
+
+# output --------------------------------------------------------------------------------------
+
+# g-comp (sl estimate)
+# unadjusted cox model
+# tmle & ic
+
+
+
