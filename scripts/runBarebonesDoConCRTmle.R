0ac528b3d75a6c50850786774af38fe9f5a1c931
David Chen <david.chen49@berkeley.edu>
2022-03-24 16:07:16 +0100

    Require Helene's continuousTMLE git repo
    deleted all copies of Helene's functions. Will depend on cloning her repo

--- scripts/runBarebonesDoConCRTmle.R
+++ scripts/runBarebonesDoConCRTmle.R
@@ -1,8 +1,10 @@
-library(survival); library(zoo); library(prodlim)
+library(survival); library(zoo); library(prodlim); library(nleqslv)
 # library(foreach); library(doRNG); library(doParallel)
 library(tidyverse); library(data.table)
 library(sl3); library(origami)
 try(setwd(dir = "/Shared/Projects/ConCR-TMLE/"))
+lapply(paste0("R/", list.files("R/")), source)
+try(setwd(dir = "/Shared/Projects/continuousTMLE/"))
 lapply(paste0("R/", list.files("R/")), source)
 
 data <- as.data.table(survival::pbc)
@@ -29,11 +31,8 @@ e_net <- make_learner(Lrnr_glmnet, alpha = 0.5)
 a_lrnrs <- logreg # make_learner(Stack, logreg, lasso, ridge, e_net)
 
 Models <- list("Trt" = a_lrnrs,
-               "0" = list(mod1 = Surv(Time, Event == 0) ~ Trt,
-                          mod3 = Surv(Time, Event == 0) ~ Trt*age + Trt*sex),
-               "1" = list(mod1 = Surv(Time, Event == 1) ~ Trt,
-                          mod2 = Surv(Time, Event == 1) ~ Trt + age + sex,
-                          mod3 = Surv(Time, Event == 1) ~ Trt*age + Trt*sex))
+               "0" = list(mod1 = Surv(Time, Event == 0) ~ Trt + age + sex),
+               "1" = list(mod1 = Surv(Time, Event == 1) ~ Trt + age + sex))
 
 estimation <- list("cause1" = list(fit = "cox",
                                    model = Surv(time, status == 1) ~ trt + age + sex),
@@ -58,12 +57,11 @@ output <- doConCRTmle(EventTime = data$time,
                       GComp = TRUE)
 
 contmle_output <- contmle(dt = data,
-                          target = 1,
+                          target = "1",
                           iterative = FALSE,
                           treat.effect = 1,
                           tau = 1200,
                           estimation = estimation,
                           treat.model = trt ~ sex + age,
-                          sl.models = list(mod1 = list(Surv(time, status == 1) ~ trt),
-                                           mod2 = list(Surv(time, status == 1) ~ trt + age + sex))
+                          sl.models = list(mod1 = list(Surv(time, status == 1) ~ trt + age + sex))
                           )
